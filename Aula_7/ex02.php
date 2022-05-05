1f29d86aaa6a9a93ecb43961701d0ce1d9ed8988
gabrielprns <gabrielpedrornn@outlook.com>
2022-05-05 10:50:42 -0300

    Project

---
+++ Aula_7/ex02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+  $a=3;
+  $b="3";
+  $res=($a===$b)?"sim":"não";
+  echo "Os valores são iguais?, $res";
+  ?>
+  
+</body>
+</html>
