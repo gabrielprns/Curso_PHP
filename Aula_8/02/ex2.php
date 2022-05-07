f2985675fd211baa1dcd603389ad91fcde714f30
gabrielprns <gabrielpedrornn@outlook.com>
2022-05-07 07:22:36 -0300

    Project

--- Aula_8/02/ex2.php
+++ Aula_8/02/ex2.php
@@ -1,8 +1,8 @@
 <?php
-$nome=$_GET["Nome"];
-$ano=$_GET["ano"];
-$sexo=$_GET["sexo"];
-$idade= date("Y")-$ano;
+$nome=isset($_GET["Nome"])?$_GET["Nome"]:"Valor não informado";
+$ano=isset($_GET["ano"])?$_GET["ano"]:"0";
+$sexo=isset($_GET["sexo"])?$_GET["sexo"]:"Valor não informado";
+$idade= isset($_GET["ano"])?date("Y")-$ano:"Valor não informado";
 echo" <br/> $nome tem $idade anos e pertence ao sexo $sexo";
 
 ?>
