131a2df724361bd0e488720d4417b7ea3573cd11
gabrielprns <gabrielpedrornn@outlook.com>
2022-05-12 08:43:55 -0300

    Project

---
+++ Aula_10/ex02.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+  <?php
+  $d = isset($_GET["ds"])?$_GET["ds"]:0;
+  switch ($d) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      echo "Levanta e vai estudar!!!";
+      break;
+    case 7:
+    case 8:
+      echo "hora de descansar";
+      break;
+    default:
+      echo "Dia da semana inválido";
+      
+  }
+  ?>
+  <br/><a href="javascript:history.go(-1)" class="botao">Voltar</a>
+  </div>
+</body>
+</html>
