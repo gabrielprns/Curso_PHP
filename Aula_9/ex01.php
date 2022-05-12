dc077721fc9b0b925bda0f8215f38ca67aadf51d
gabrielprns <gabrielpedrornn@outlook.com>
2022-05-12 05:57:11 -0300

    Project

--- Aula_9/ex01.php
+++ Aula_9/ex01.php
@@ -2,4 +2,14 @@
 $a = isset($_GET["ano"])?$_GET["ano"]:1900;
 $i = date("Y")-$a;
 echo "Você nasceu em $a e tem $i anos";
+
+if($i>=18 && $i<=65){
+  $per = "seu Voto é obrigatório";
+}elseif(($i>=16 & $i<=18) || ($i>65)){
+  $per = "seu Voto é opcional";
+}else{
+  $per = "Não Vota";
+}
+echo " </br> Com essa idade $per"
+
 ?>
