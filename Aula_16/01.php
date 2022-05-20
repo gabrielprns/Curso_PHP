8ea003305ae2ab87ef199bdb31b8c7a4777a0462
gabrielprns <gabrielpedrornn@outlook.com>
2022-05-20 09:45:15 -0300

    Project

---
+++ Aula_16/01.php
@@ -0,0 +1,5 @@
+<?php
+$p = "Leite";
+$pr= 5.5;
+printf ("O %s custa %.2f", $p, $pr);
+?>
---
+++ Aula_16/02.php
@@ -0,0 +1,9 @@
+<?php
+$v[0]=77;
+$v[1]="B";
+$v[2]="sihfdhf";
+print_r($v);
+$vr= array(5,7,4,2,7);
+echo"<br/>";
+print_r($vr);
+?>
